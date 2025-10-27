89c61e03ac8a94089e1b4ec26c8e21f8081d9f2f
Diego Licea <dlicea@uw.edu>
2025-10-26 23:50:20 -0700

    Added static round progress bars

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -3,13 +3,17 @@ registerSketch('sk2', function (p) {
 
   let roundTimeMs = 300000;
   let timeElapsed;
+  let round = 1;
 
+  //display constants
   const fighter1Surname = "NURMAGOMEDOV";
   const fighter2Surname = "GAETHJE";
   const fighter1Color = 'red';
   const fighter2Color = 'blue';
-
   const fightTitle = 'LIGHTWEIGHT BOUT';
+
+  const roundBarWidth = 400; //used in step 3 for logic and drawing
+  
 
   p.preload = function(){
     logoFont = p.loadFont('fonts/sternbach.otf'); // preload font
@@ -29,9 +33,9 @@ registerSketch('sk2', function (p) {
     const middleWidth = p.windowWidth/2;
     const middleHeight = p.windowHeight/2;
 
-    //STEP 1. middle rectangle graphic w/ time and logo-----------------
+    //STEP 1. dark gray middle graphic w/ time and logo-----------------
     p.fill(37, 37, 37);
-    // I split the square into 2 polygons to create a stylistic split down the middle
+    // I split the square into 2 polygons to create a cool diagonal split down the middle
     //left half
     p.beginShape();
     p.vertex(middleWidth - 500/2, middleHeight - 90/2); // top-left
@@ -53,9 +57,8 @@ registerSketch('sk2', function (p) {
     p.fill(231, 189, 1);
     p.text("AFC", middleWidth - 150, middleHeight - 10);
 
-    // 5 minute round ticker 
-    p.textFont(tickerFont);
-
+    // Time ticker
+    p.textFont(tickerFont); 
     p.push();
     const { mm, ss } = msToMMSS(roundTimeMs - timeElapsed);
     p.textSize(70);
@@ -63,7 +66,7 @@ registerSketch('sk2', function (p) {
     p.text(mm + ":" + ss, middleWidth + 120, middleHeight - 8 );
     p.pop();
 
-    //STEP 2. left and right rectangles with fighter info-----------------
+    //STEP 2. Additional fight info graphics-----------------
 
     p.noStroke();
     p.fill(37, 37, 37, 200);
@@ -83,9 +86,26 @@ registerSketch('sk2', function (p) {
     p.text(fightTitle, middleWidth, middleHeight + 77)
 
     //STEP 3. Bars that fill up per round (the hard part)----------------
-    
 
+    p.push()
+    p.fill(250, 250, 250, 190);
+    p.rect(middleWidth - 500, middleHeight - 80, roundBarWidth, 30);
+    p.rect(middleWidth + 500, middleHeight - 80, roundBarWidth, 30);
+    p.rect(middleWidth, middleHeight - 80, 400, 30);
+    p.pop();
     
+    // round text over bars depending on what round it is
+    p.textSize(30);
+    p.fill(231, 189, 1);
+    if (round == 1){
+      p.text("Round 1", middleWidth - 500, middleHeight - 120);
+    } else if (round == 2){
+      p.text("Round 2", middleWidth, middleHeight - 120);
+    } else {
+      p.text("Round 3", middleWidth + 500, middleHeight - 120);
+    }
+
+
   };
 
   //Styling functions
