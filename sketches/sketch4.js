67ac8add462b77c6186478ab88b0b1025a664db6
Diego Licea <diegol@Diegos-MacBook-Air-9.local>
2025-10-27 23:51:23 -0700

    Added fade in and out of stories feature for WASH REPORT graphic

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -20,7 +20,11 @@ registerSketch('sk4', function (p) {
   ]
 
   let timePerStoryMs = 5000;
+  const timePerStoryStandard = timePerStoryMs;
+  let fadeMs = 1000;
   let currentIndex = 0;
+
+  let alpha = 255;
 
   p.preload = function() {
     huskyLogo = p.loadImage('custom_images/white_husky_logo.png');
@@ -32,12 +36,26 @@ registerSketch('sk4', function (p) {
   };
   p.draw = function () {
     let elapsedTime = p.millis();
+    let storyElapsed = elapsedTime % timePerStoryStandard;
+
 
     if (elapsedTime >= timePerStoryMs){
       if (data[currentIndex + 1] != null){
-        timePerStoryMs += timePerStoryMs;
+        timePerStoryMs += timePerStoryStandard;
         currentIndex++;
-      } 
+      } else {
+        timePerStoryMs += timePerStoryStandard;
+        currentIndex = 0;
+      }
+    }
+
+    // fade logic: fades alpha levels in and out proportional to how far storyElapsed is from fadeMs.
+    if (storyElapsed < fadeMs){
+      alpha = p.map(storyElapsed, 0, fadeMs, 0, 255);
+    } else if (storyElapsed > timePerStoryStandard - fadeMs){
+      alpha = p.map(storyElapsed, timePerStoryStandard - fadeMs,  timePerStoryStandard, 255, 0);
+    } else {
+      alpha = 255;
     }
 
     p.background(190);
@@ -50,6 +68,8 @@ registerSketch('sk4', function (p) {
     const middleHeight = p.windowHeight * 2/2.5;
     const barWidth = 1500;
     const barHeight = 100;
+
+    
 
     //rgba(39, 39, 39, 1)
     //STEP 1. draw overlay rectangle -------------------
@@ -86,26 +106,26 @@ registerSketch('sk4', function (p) {
     let currentStory = data[currentIndex];
 
     //Sport type subsection
-    p.fill(235);
+    p.fill(235, 235, 235, alpha);
     p.stroke(240);
     p.strokeWeight(2);
     p.strokeCap(p.SQUARE);
     p.line(middleWidth - 250, middleHeight - 45, middleWidth - 250, middleHeight + 45);
     p.textSize(40);
     p.noStroke();
-    p.text(currentStory.league, middleWidth - 350, middleHeight + 5);
+    p.text(currentStory.league, middleWidth - 350, middleHeight + 3);
 
+    p.textAlign(p.LEFT);
     //Matchup
     p.textSize(50);
-    p.text(currentStory.team1 + " vs. " + currentStory.team2, middleWidth - 5, middleHeight - 10);
+    p.text(currentStory.team1 + " vs. " + currentStory.team2, middleWidth - 230, middleHeight - 10);
 
     //Time and place
     p.textSize(30);
-    p.fill(220);
+    p.fill(220, 220, 220, alpha);
     p.text(currentStory.timeAndPlace, middleWidth + 380, middleHeight - 10);
     //Story
     p.push()
-    p.textAlign(p.LEFT);
     p.textSize(18);
     p.textStyle(p.ITALIC);
     p.text(currentStory.storyText, middleWidth - 230, middleHeight + 30);
