3d0a33b1c2a95eaef53f6dd301f72596a685522b
Diego Licea <dlicea@uw.edu>
2025-10-31 16:00:19 -0700

    Added array of objects data for color schemes based on team selected

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,5 +1,33 @@
 // Example 2
 registerSketch('sk5', function (p) {
+
+
+  // Color schemes based on the team that the user chooses
+  const teamColors = [
+    {
+      team:"Seahawks",
+      background: 'rgb(0, 0, 0)',
+      secondaryColor: 'rgb(0, 0, 0)'
+    },
+    {
+      team:"Broncos",
+      background: 'rgb(0, 0, 0)',
+      secondaryColor: 'rgb(0, 0, 0)'
+    },
+    {
+      team:"Steelers",
+      background: 'rgb(0, 0, 0)',
+      secondaryColor: 'rgb(0, 0, 0)'
+    },
+    {
+      team:"Giants",
+      background: 'rgb(0, 0, 0)',
+      secondaryColor: 'rgb(0, 0, 0)'
+    }
+  ]
+
+  const currentTeam = "Seahawks";
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
@@ -7,17 +35,27 @@ registerSketch('sk5', function (p) {
   p.draw = function () {
     p.background(250);
 
-    // Corner time display
-    const h = p.hour();
-    const m = p.minute();
-    const s = p.second();
-    const label = p.nf(h, 2) + ':' + p.nf(m, 2) + ':' + p.nf(s, 2);
+    const middleWidth = p.windowWidth/2;
+    const middleHeight = p.windowHeight/2;
 
-    p.noStroke();
-    p.fill(20);
-    p.textAlign(p.LEFT, p.TOP);  // change to RIGHT/BOTTOM for other corners
-    p.textSize(50);
-    p.text(label, 12, 10);   // top-left corner
+    //STEP 1 Background Coloring------------------------
+
+    //sets background and stroke colors to whatever the currently selected team is in teamColors
+    p.background(teamColors.find(object => object.team === currentTeam).background); 
+
+    //STEP 2 Set divides between both players-------------------
+
+
+
+    // STEP 3 Data bars for each player
+
+
+
+    // STEP 4 Contextual text
+
+
+
+
 
   }
 
