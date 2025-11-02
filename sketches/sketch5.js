ab722fe446a1ed2cebe06e35df1a24528f74bad5
Diego Licea <dlicea@uw.edu>
2025-11-01 20:57:39 -0700

    Static version of stat bars and player names

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -7,7 +7,10 @@ registerSketch('sk5', function (p) {
     {
       team:"Seahawks",
       background: 'rgb(0, 34, 68)',
-      secondaryColor: 'rgb(105, 190, 40)'
+      secondaryColor: 'rgb(105, 190, 40)',
+      russLeaveText: "Wilson left the Seahawks after 2021",
+      replacementText: "Smith started as QB for Seattle starting 2022",
+      russLastAppearance: "2021"
     },
     {
       team:"Broncos",
@@ -28,38 +31,37 @@ registerSketch('sk5', function (p) {
 
   const currentTeam = "Seahawks";
 
+  p.preload = function () {
+    //career passing data for all 4 relevant qb's (Russel Wilson, and the 3 people that have replaced him);
+    normalFont = p.loadFont('fonts/Tomorrow-Regular.ttf');
+    boldFont = p.loadFont('fonts/Tomorrow-SemiBoldItalic.ttf');
+
+    russData = p.loadTable('data_sets/homework5/RusselWilson.csv', 'csv', 'header');
+    nixData = p.loadTable('data_sets/homework5/BoNix.csv', 'csv', 'header');
+    fieldsData = p.loadTable('data_sets/homework5/JustinFields.csv', 'csv', 'header');
+    genoData = p.loadTable('data_sets/homework5/GenoSmith.csv', 'csv', 'header');
+    dartData = p.loadTable('data_sets/homework5/JaxsonDart.csv', 'csv', 'header');
+  }
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
 
-  p.preload = function () {
-    //career passing data for all 4 relevant qb's (Russel Wilson, and the 3 people that have replaced him);
-
-    const russData = p.loadTable('data_sets/homework5/RusselWilson.csv');
-    const nixData = p.loadTable('data_sets/homework5/BoNix.csv');
-    const fieldsData = p.loadTable('data_sets/homework5/JustinFields.csv');
-    const genoData = p.loadTable('data_sets/homework5/genoData.csv');
-    const dartData = p.loadTable('data_sets/homework5/genoData.csv');
-
-    normalFont = p.loadFont('fonts/Tomorrow-Regular.ttf');
-    boldFont = p.loadFont('fonts/Tomorrow-SemiBoldItalic.ttf');
-  }
-
   p.draw = function () {
-    p.background(250);
 
     const middleWidth = p.windowWidth/2;
     const middleHeight = p.windowHeight/2;
 
     //STEP 1 Background Coloring------------------------
-
+    let mainColor = teamColors.find(object => object.team === currentTeam).background;
+    let secondaryColor = teamColors.find(object => object.team === currentTeam).secondaryColor;
     //sets background and stroke colors to whatever the currently selected team is in teamColors
-    p.background(teamColors.find(object => object.team === currentTeam).background); 
+    p.background(mainColor); 
 
     //STEP 2 Set divides between both players-------------------
 
     p.stroke(255);
-    p.strokeWeight(3);
+    p.strokeWeight(5);
     p.strokeCap(p.SQUARE);
     p.line(middleWidth, middleHeight - 400, middleWidth, middleHeight + 400);
 
@@ -68,16 +70,54 @@ registerSketch('sk5', function (p) {
     p.noStroke();
     p.textFont(normalFont);
     p.fill(255);
-    p.textSize(25);
+    p.textSize(28);
     p.textAlign(p.CENTER);
-    p.text("QBR", middleWidth, middleHeight - 150);
-    p.text("Completion%", middleWidth, middleHeight - 50);
-    p.text()
-    p.text("Yards per", middleWidth, middleHeight + 170);
-      p.text("Attempt", middleWidth, middleHeight + 140);
+
+ 
+
+    // add gaps in the center line
+    p.fill(mainColor);
+    p.rectMode(p.CENTER);
+    p.rect(middleWidth, middleHeight - 250, 10, 70);
+    p.rect(middleWidth, middleHeight + 245, 10, 70);
+    p.rect(middleWidth, middleHeight - 90, 10, 70);
+    p.rect(middleWidth, middleHeight + 70, 10, 70);
+    p.fill(255);
+
+    // stat text
+    p.text("QBR", middleWidth, middleHeight - 240);
+    p.textSize(24)
+    p.text("Completion%", middleWidth, middleHeight - 80);
+    p.text("Touchdowns", middleWidth, middleHeight + 80);
+    p.text("Yards per", middleWidth, middleHeight + 240);
+      p.text("Attempt", middleWidth, middleHeight + 270);
+
+
+    p.rectMode(p.CORNER);
+    p.fill(secondaryColor);
+
+    //QBR
+    p.rect(middleWidth + 95, middleHeight - 265, 250, 30);
+    p.rect(middleWidth - 95 - 250, middleHeight - 265, 250, 30);
+
+    //Comp%
+    p.rect(middleWidth + 95, middleHeight - 105, 250, 30);
+    p.rect(middleWidth - 95 - 250, middleHeight - 105, 250, 30);
+
+    //Touchdowns
+    p.rect(middleWidth + 95, middleHeight + 55, 250, 30);
+    p.rect(middleWidth - 95 - 250, middleHeight + 55, 250, 30);
+
+    //Yards per Attempt
+    p.rect(middleWidth + 95, middleHeight + 230, 250, 30);
+    p.rect(middleWidth - 95 - 250, middleHeight + 230, 250, 30);
 
 
     // STEP 4 Contextual text
+
+    p.textSize(70);
+    p.text("Russel Wilson", middleWidth - 670, middleHeight - 360);
+    p.text("Geno Smith", middleWidth + 670, middleHeight - 360);
 
 
   }
